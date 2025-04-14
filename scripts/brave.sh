85f4e1970e43282e0694b19a92e99cb5708360ae
nadeken <musiclaboratory0x1b8@gmail.com>
2025-04-14 09:12:04 +0900

    brave version changed

--- scripts/brave.sh
+++ scripts/brave.sh
@@ -1,1 +1,2 @@
-curl -fsS https://dl.brave.com/install.sh | sh
+wget https://github.com/brave/brave-browser/releases/download/v1.75.170/brave-browser-beta_1.75.170_amd64.deb
+sudo apt install ./brave-browser-beta_1.75.170_amd64.deb
