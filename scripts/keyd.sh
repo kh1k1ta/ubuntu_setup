98cee34e1a597b41390d8e54450cbbb0d98cdfc5
nadeken <musiclaboratory0x1b8@gmail.com>
2025-04-18 05:19:24 +0900

    keyd update

--- scripts/keyd.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/rvaiya/keyd
-cd keyd
-sudo make && sudo make install
-sudo systemctl enable keyd
-sudo systemctl start keyd
-cd ..
-sudo cp ./imports/default.conf /etc/keyd/default.conf
-sudo keyd reload
-
---
+++ scripts/obsidian.sh
@@ -0,0 +1,2 @@
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.8.9/obsidian_1.8.9_amd64.deb
+sudo apt install ./obsidian_1.8.9_amd64.deb
