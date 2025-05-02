43dcea1dbe306f4b82230e3b7a1c0e1c2eb1cc1e
nadeken <musiclaboratory0x1b8@gmail.com>
2025-05-02 11:29:55 +0900

    keyd config change

--- scripts/keyd.sh
+++ scripts/keyd.sh
@@ -1,9 +1,17 @@
 git clone https://github.com/rvaiya/keyd
 cd keyd
 sudo make && sudo make install
+
+clear
 sudo systemctl enable keyd
 sudo systemctl start keyd
+
+clear
 cd ..
-sudo cp ./imports/default.conf /etc/keyd/default.conf
+cp -r ./imports/keyd /home/rei/setting/
+sudo rm -r /etc/keyd
+sudo ln -s /home/rei/setting/keyd /etc/keyd
+
+clear
 sudo keyd reload
 sudo rm -rf keyd
