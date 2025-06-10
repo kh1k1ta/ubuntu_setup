89a501655ae571c7c3219263148f7901d4982523
nadeken <musiclaboratory0x1b8@gmail.com>
2025-06-10 14:31:34 +0900

    cui version

--- scripts/minecraft.sh
+++
@@ -1,3 +0,0 @@
-curl https://launcher.mojang.com/download/Minecraft.deb -o Minecraft.deb
-sudo apt install ./Minecraft.deb -y
-rm Minecraft.deb
--- scripts/yt-dlp.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ~/.local/bin
-source .profile
-wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O ~/.local/bin/yt-dlp
-chmod +x ~/.local/bin/yt-dlp
-wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O ~/.local/bin/yt-dlp
-chmod +x ~/.local/bin/yt-dlp
-
